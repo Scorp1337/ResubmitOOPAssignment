b8f16b2b3ecce1bbb4a78a295ad8269807e0620c
Ionut G Scorp <ionutgscorp@gmail.com>
2023-03-10 17:52:20 +0200

    Added a new method in Class Book with a variable String typeOfBook that will let us introduce what kind of book to add to our list.

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -8,17 +8,10 @@ public class Book {
     public int numberOfPages;
 
 
-
-
-
     public Book(String name, int numberOfPages) {
         this.name = name;
         this.numberOfPages = numberOfPages;
     }
-
-
-
-
 
     @Override
     public String toString() {
@@ -28,18 +21,19 @@ public class Book {
                 ']';
     }
 
-    public void addBooks(String typeOfBook){
+    public void addBooks(String typeOfBook) {
 
 
-        System.out.println("You added a book of type: " + typeOfBook );
+        System.out.println("You added a book of type: " + typeOfBook);
 
 
     }
 
-    public void deleteBook(){
+    public void deleteBook() {
         this.name = null;
         System.out.println("Book deleted " + this.name);
     }
+
     public static void printBooks(List<Book> books) {
         for (Book book : books) {
             System.out.println(book);
